194d5a8e9e6dcab0fb1bfed528b4f8faecc4cbfd
benjaminboruff <11185240+benjaminboruff@users.noreply.github.com>
2025-09-07 15:38:19 -0700

    alphanum

--- cpp/chap1/lexer.cpp
+++ cpp/chap1/lexer.cpp
@@ -1,3 +1,5 @@
+#include <cctype>
+#include <cstdio>
 #include <string>
 // The lexer returns tokens [0-255] if it is an unknown character, otherwise one
 // of these for known things.
@@ -17,10 +19,36 @@ static std::string IdentifierStr; // Filled in if tok_identifier
 static double NumVal;             // Filled in if tok_number
 
 /// gettok - Return the next token from standard input
-static int gettok() { return 0; }
+static int gettok() {
+  static int LastChar = ' ';
+
+  // Skip any whitespace
+  while (isspace(LastChar))
+    LastChar = getchar();
+
+  if (isalnum(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]*
+    IdentifierStr = LastChar;
+    while (isalnum(LastChar = getchar()))
+      IdentifierStr += LastChar;
+
+    if (IdentifierStr == "def")
+      return tok_def;
+    if (IdentifierStr == "extern")
+      return tok_extern;
+    return tok_identifier;
+  }
+
+  return 0;
+}
 
 int main() {
+  std::string token;
 
   fprintf(stderr, "ready> ");
+  token = gettok();
+
+  fprintf(stderr, "The token is: %s", token.c_str());
+  fprintf(stderr, "The token is: %s", IdentifierStr.c_str());
+
   return 0;
 }
