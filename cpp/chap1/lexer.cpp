c7aba7283e277e04e55fc52f5b820e1a08c98bb7
benjaminboruff <11185240+benjaminboruff@users.noreply.github.com>
2025-09-08 16:21:49 -0700

    lexer working in test loop

--- cpp/chap1/lexer.cpp
+++ cpp/chap1/lexer.cpp
@@ -1,5 +1,6 @@
 #include <cctype>
 #include <cstdio>
+#include <cstdlib>
 #include <string>
 // The lexer returns tokens [0-255] if it is an unknown character, otherwise one
 // of these for known things.
@@ -26,7 +27,7 @@ static int gettok() {
   while (isspace(LastChar))
     LastChar = getchar();
 
-  if (isalnum(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]*
+  if (isalpha(LastChar)) { // identifier: [a-zA-Z][a-zA-Z0-9]*
     IdentifierStr = LastChar;
     while (isalnum(LastChar = getchar()))
       IdentifierStr += LastChar;
@@ -38,17 +39,50 @@ static int gettok() {
     return tok_identifier;
   }
 
-  return 0;
+  if (isdigit(LastChar) || LastChar == '.') { // LastChar Number: [0-9.]+
+    std::string NumStr;
+    do {
+      NumStr += LastChar;
+      LastChar = getchar();
+    } while (isdigit(LastChar) || LastChar == '.');
+
+    NumVal = std::strtod(NumStr.c_str(), 0);
+    return tok_number;
+  }
+
+  if (LastChar == '#') {
+    // Comment until the eol
+    do {
+      LastChar = getchar();
+    } while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+    if (LastChar != EOF)
+      return gettok();
+  }
+
+  // Check for EOF. Don't eat the EOF
+  if (LastChar == EOF)
+    return tok_eof;
+
+  // Otherwise just return the character as it's ascii value
+  int ThisChar = LastChar;
+  LastChar = getchar();
+  return ThisChar;
 }
 
 int main() {
-  std::string token;
+  int token;
 
   fprintf(stderr, "ready> ");
-  token = gettok();
 
-  fprintf(stderr, "The token is: %s", token.c_str());
-  fprintf(stderr, "The token is: %s", IdentifierStr.c_str());
+  while (token != -1) {
+
+    token = gettok();
+
+    fprintf(stderr, "The token is: %d\n", token);
+    fprintf(stderr, "The identifier is: %s\n", IdentifierStr.c_str());
+    fprintf(stderr, "The NumVal is: %f\n", NumVal);
+  }
 
   return 0;
 }
