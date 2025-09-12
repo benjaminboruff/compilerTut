288c2444f45222c914e39eb56311efb2817d2597
benjaminboruff <11185240+benjaminboruff@users.noreply.github.com>
2025-09-11 17:19:17 -0700

    AST classes defined

--- cpp/chap2/compiler.cpp
+++ cpp/chap2/compiler.cpp
@@ -1,7 +1,10 @@
 #include <cctype>
 #include <cstdio>
 #include <cstdlib>
+#include <memory>
 #include <string>
+#include <utility>
+#include <vector>
 
 //===---------------------------------------------------------------------===//
 // Lexer
@@ -91,10 +94,51 @@ class NumberExprAST : public ExprAST {
   double Val;
 
 public:
-  NumberExprAST(double initialVal) : Val(initialVal) {}
+  NumberExprAST(double Val) : Val(Val) {}
 };
-} // namespace
 
+/// VariableExprAST - Expression class for referencing a variable, like "a".
+class VariableExprAST : public ExprAST {
+  std::string Name;
+
+public:
+  VariableExprAST(const std::string &Name) : Name(Name) {}
+};
+
+/// BinaryExprAST - Expression class for binary operator.
+class BinaryExprAST : public ExprAST {
+  char Op;
+  std::unique_ptr<ExprAST> LHS, RHS;
+
+public:
+  BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS,
+                std::unique_ptr<ExprAST> RHS)
+      : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
+
+/// CallExprAST - Expression class for function calls.
+class CallExprAST : public ExprAST {
+  std::string Callee;
+  std::vector<std::unique_ptr<ExprAST>> Args;
+
+public:
+  CallExprAST(const std::string &Callee,
+              std::vector<std::unique_ptr<ExprAST>> Args)
+      : Callee(Callee), Args(std::move(Args)) {}
+};
+} // end anonymous namespace
+
+//===-----------------------------------------------------------===//
+// Parser
+//===-----------------------------------------------------------===//
+
+//===-----------------------------------------------------------===//
+// Top-Level parsing
+//===-----------------------------------------------------------===//
+
+//===-----------------------------------------------------------===//
+// Main driver code.
+//===-----------------------------------------------------------===//
 int main() {
   int token;
 
